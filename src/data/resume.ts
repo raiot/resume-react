c029a3e9f4006ebb8bfedfb79dd0c2348ffce354
Rene Alberto Durazo Morales <renedurazo@Renes-MacBook-Pro.local>
2024-02-26 16:57:37 -0700

    update spelling

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -1,14 +1,14 @@
 'use client'
 
 const resume: Resume = {
-  summary: 'I\'m a Senior Software Developer With 10+ years of experience, I have worked across multiple product types, I love learning and trying new things and I can adapt to any technology needed. I like to take part in decision making and meaninful conversations. I like to **get things done** to the best of my extent and expetise.',
+  summary: 'I\'m a Senior Software Developer With 10+ years of experience, I have worked across multiple product types, I love learning and trying new things and I can adapt to any technology needed. I like to take part in decision-making and meaningful conversations. I like to get things done to the best of my extent and expertise.',
   experiences: [
     {
       position: 'Senior Software Developer, (Frontend)',
       company: 'Wizeline',
       location: 'Remote',
       time: '2022 - current',
-      description: `As a Senior Software Developer for Wizeline I was involved in planning, artchitecture, deployment and development of a new platform.
+      description: `As a Senior Software Developer for Wizeline I was involved in the planning, architecture, deployment and development of a new platform.
       **Highlights** include:\
       - Leading the development of a new platform
       - Working with a team of 5+ developers
@@ -31,7 +31,7 @@ const resume: Resume = {
       company: 'Skupos',
       location: 'Remote',
       time: '2021 - 2022',
-      description: 'As a Senior Software Developer for Skupos, I was involved in planning and estimation for new features, I worked closely with Product and Marketing to setup campaing goals and stadistics, set up a few goals in Google Analytics, approved and reviewed PRs and releases in AWS Pipelines. **Highlights** include: - Leading a new internal design system - Working with Product and Marketing to define goals and setup external tools - Implementing a new CI/CD pipeline with Github Actions for the internal design system - Implementing Cypress from scratch from our webapp with 80% coverage.',
+      description: 'As a Senior Software Developer for Skupos, I was involved in planning and estimation for new features, I worked closely with Product and Marketing to set campaign goals and statistics, set up a few goals in Google Analytics, approved and reviewed PRs and releases in AWS Pipelines. **Highlights** include: - Leading a new internal design system - Working with Product and Marketing to define goals and set external tools - Implementing a new CI/CD pipeline with GitHub Actions for the internal design system - Implementing Cypress from scratch from our web app with 80% coverage.',
       technologies: [
         'React',
         'Material UI',
@@ -56,7 +56,7 @@ const resume: Resume = {
       company: 'Mindstrong',
       location: 'Remote',
       time: '2019 - 2021',
-      description: 'As a Senior Software Developer for Mindstrong, I was involved in planning and estimation for new features, I worked closely with Product to setup campaing goals and stadistics, set up copies of the site for A/B testing, approved and reviewed PRs and releases in AWS Pipelines. **Highlights** include: - Set up Google Optimize with several copies for A/B testing - Worked with Product to define goals, funnels and other metrics using Google Analytis - Lead the frontend implementation of a new platform to discover potential leads - Increased Cypress automation test coverage by 20%.',
+      description: 'As a Senior Software Developer for Mindstrong, I was involved in planning and estimation for new features, I worked closely with Product to set campaign goals and statistics, set up copies of the site for A/B testing, approved and reviewed PRs and releases in AWS Pipelines. **Highlights** include: - Set up Google Optimize with several copies for A/B testing - Worked with Product to define goals, funnels and other metrics using Google Analytics - Led the frontend implementation of a new platform to discover potential leads - Increased Cypress automation test coverage by 20%.',
       technologies: [
         'TypeScript',
         'React',
@@ -77,7 +77,7 @@ const resume: Resume = {
       company: 'Nextiva',
       location: 'Guadalajara, México',
       time: '2018 - 2019',
-      description: 'As a Senior Software Developer for Nextiva, I was involved in improving the security and performance of the app. **Highlights** include: - Lead initiaive to improve deploy performance, decreasing 60% build time - Worked with Backend to set up session using JWToken  - Add test 70% of test coverage. - Setup nginx to avoid COORS issues in internal apps.',
+      description: 'As a Senior Software Developer for Nextiva, I was involved in improving the security and performance of the app. **Highlights** include: - Lead initiative to improve deployment performance, decreasing 60% build time - Worked with Backend to set up session using JWToken  - Add test 70% of test coverage. - Setup nginx to avoid COORS issues in internal apps.',
       technologies: [
         'TypeScript',
         'React',
@@ -96,7 +96,7 @@ const resume: Resume = {
       company: 'HomeAway (Vrbo/Expedia)',
       location: 'Guadalajara, México',
       time: '2016 - 2018',
-      description: 'As a Senior Software Developer for HomrAway, I was involved in migrating the onboarding flow and the property managment flow from Java/Spring/JSP to React/Typescript/Node. **Highlights** include: - Debugging crucial parts of the property state machine.',
+      description: 'As a Senior Software Developer for HomrAway, I was involved in migrating the onboarding flow and the property management flow from Java/Spring/JSP to React/Typescript/Node. **Highlights** include: - Debugging crucial parts of the property state machine.',
       technologies: [
         'TypeScript',
         'React',
@@ -122,7 +122,7 @@ const resume: Resume = {
       {
         name: 'SongBox.webapp',
         url: 'https://github.com/tepachelabs/songbox.webapp',
-        description: 'Listeng to your music collection from the cloud (Dropbox)',
+        description: 'Listening to your music collection from the cloud (Dropbox)',
       }
     ],
   },
